8fb41461c4f6ea522a64974ee3fc59b316ca6d46
Krino Suterio <suteriokrino@gmail.com>
2016-12-10 18:01:56 +0800

    Create mini_calc.cpp

---
+++ mini_calc.cpp
@@ -0,0 +1,31 @@
+ #include<iostream>
+using namespace std;
+int sum(int a, int b)
+{
+	return a+b;
+}
+int diff(int a,int b)
+{
+	return a-b;
+}
+int product(int a, int b)
+{
+	return a*b;
+}
+int quotient(int a,int b)
+{
+	return a/b;
+}
+int main()
+{	
+	int a,b;
+	cout <<"Enter number 1: ";
+	cin>>a;
+	cout <<"Enter number 2: ";
+	cin>>b;
+	cout<<"Sum is:"<<sum(a,b)<<endl;
+	cout<< "Difference is:"<<diff(a,b)<<endl;
+	cout<<"Product is:"<<product(a,b)<<endl;
+	cout<< "Quotient us :"<<quotient(a,b)<<endl;
+	return 0;	
+}
