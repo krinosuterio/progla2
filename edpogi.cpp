fc51d6876038c22f9db2a2a48a4d918886b1dabe
Krino Suterio <suteriokrino@gmail.com>
2016-12-10 18:41:50 +0800

    Update edpogi.cpp

--- edpogi.cpp
+++ edpogi.cpp
@@ -4,18 +4,20 @@ using namespace std;
 int main()
 {	
 for (int x=1;x<=100;x++)
-{
+{	
+	
 	if (x%3==0)
 	{
 		cout <<"Pogi"<<endl;
 	}
-	if (x%5==0)
+	else if (x%5==0)
 	{
 		cout<< "Ed"<<endl;
 	}
-	
-	
+	else
+	{
 	cout <<x<<endl;
+	}
 	
 }
 	return 0;	
