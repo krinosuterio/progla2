5ec6f4bb3bcd2509a61af50d8d99ff2e4aba337d
Krino Suterio <suteriokrino@gmail.com>
2016-12-10 18:36:34 +0800

    Create edpogi.cpp

---
+++ edpogi.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{	
+for (int x=1;x<=100;x++)
+{
+	if (x%3==0)
+	{
+		cout <<"Pogi"<<endl;
+	}
+	if (x%5==0)
+	{
+		cout<< "Ed"<<endl;
+	}
+	
+	
+	cout <<x<<endl;
+	
+}
+	return 0;	
+}
