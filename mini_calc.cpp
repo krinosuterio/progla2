2cc800a35e69d718f277f656426a56b9df35984d
Krino Suterio <suteriokrino@gmail.com>
2016-12-10 18:02:42 +0800

    Update mini_calc.cpp

--- mini_calc.cpp
+++ mini_calc.cpp
@@ -26,6 +26,6 @@ int main()
 	cout<<"Sum is:"<<sum(a,b)<<endl;
 	cout<< "Difference is:"<<diff(a,b)<<endl;
 	cout<<"Product is:"<<product(a,b)<<endl;
-	cout<< "Quotient us :"<<quotient(a,b)<<endl;
+	cout<< "Quotient is:"<<quotient(a,b)<<endl;
 	return 0;	
 }
