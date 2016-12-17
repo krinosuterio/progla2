a6a89eb552192a53e31ca12479ef082dbe8a8624
Krino Suterio <suteriokrino@gmail.com>
2016-12-17 16:45:13 +0800

    Update structure.cpp

--- structure.cpp
+++ structure.cpp
@@ -1,11 +1,16 @@
 #include<iostream>
 using namespace std;
 struct {
+	int toes;
 	int knees;
 	int shoulder;
-	int head
+	int head;
 }Person ;
 int main()
 {
+	cin>>Person.knees;	
+	cin>>Person.shoulder;
+	cin>>Person.head;
+	cin>>Person.toes;
 	return 0;
 }
